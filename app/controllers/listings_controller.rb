fb8c3b8d7eac15f608ad1f35f7e902002b41dd8c
Greg Martin <gregm31@live.com>
2020-05-12 14:55:01 +1000

    Created new and show pages to listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,6 +6,25 @@ class ListingsController < ApplicationController
     end
 
     def new
-    
+        @listing = Listing.new
     end
+
+    def create
+        @listing = current_user.listings.create(listing_params)
+        if @listing.errors.any?
+            render "new"
+        else
+            redirect_to listings_path
+        end
+    end
+
+    def show
+        @listing = Listing.find(params[:id])
+    end
+
+    private
+    def listing_params
+        params.require(:listing).permit(:title, :description, :price, :picture)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get "/listings", to: "listings#index", as: "listings"
 
   get "/listings/new", to: "listings#new", as: "new_listing"
+
+  post "/listings", to: "listings#create"
+  get "/listings/:id", to: "listings#show", as: "listing"
 end
