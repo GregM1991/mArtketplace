838218396cd94370040e725da5bdb66967442022
Greg Martin <gregm31@live.com>
2020-05-14 09:55:09 +1000

    Added stripe, and donate buttons to app

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,6 +3,27 @@ class ListingsController < ApplicationController
 
     def index
         @listings = Listing.all
+
+        session = Stripe::Checkout::Session.create(
+            payment_method_types: ['card'],
+            customer_email: current_user.email,
+            line_items: [{
+                name: "Donate to the mArtketplace!",
+                amount: 1000,
+                currency: 'aud',
+                quantity: 1,
+            }],
+            payment_intent_data: {
+                metadata: {
+                    user_id: current_user.id,
+                }
+            },
+            success_url: "#{root_url}pages/donated?userId=#{current_user.id}",
+            cancel_url: "#{root_url}"
+        )
+    
+        @session_id = session.id
+
     end
 
     def new
@@ -18,11 +39,46 @@ class ListingsController < ApplicationController
         end
     end
 
+    def edit
+        @listing = current_user.listings.find_by_id(params[:id])
+
+        if @listing
+            render("edit")
+        else
+            redirect_to listing_path
+        end
+    end
+
+    def update
+        @listing = current_user.listings.find_by_id(params[:id])
+
+        if @listing 
+            @listing.update(listing_params)
+            if @listing.errors.any?
+                render "edit"
+            else
+                redirect_to listing_path
+            end
+        else
+            redirect_to listing_path
+        end
+    end
+
     def show
         @listing = Listing.find(params[:id])
     end
 
+    def destroy
+        @listing = current_user.listings.find_by_id(params[:id])
+
+        if @listing
+            @listing.destroy
+        end
+        redirect_to listings_path
+    end
+
     private
+
     def listing_params
         params.require(:listing).permit(:title, :description, :price, :picture)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get "/", to: "pages#home", as: "root" 
 
   get "/listings", to: "listings#index", as: "listings"
-
   get "/listings/new", to: "listings#new", as: "new_listing"
-
   post "/listings", to: "listings#create"
   get "/listings/:id", to: "listings#show", as: "listing"
+  get "/listings/:id/edit", to: "listings#edit", as: "edit_listing"
+  put "/listings/:id", to: "listings#update"
+  patch "/listings/:id", to: "listings#update"
+  delete "/listings/:id", to: "listings#destroy"
 end
