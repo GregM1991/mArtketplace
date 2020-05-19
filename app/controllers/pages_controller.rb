a45ce193f2f79a998e709dd2e8ce8660e98964be
Greg Martin <gregm31@live.com>
2020-05-19 15:38:40 +1000

    Fixing stripe email null bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,24 +10,26 @@ class PagesController < ApplicationController
     private
 
     def generate_stripe_session
-        session = Stripe::Checkout::Session.create(
-            payment_method_types: ['card'],
-            customer_email: current_user.email,
-            line_items: [{
-                name: "Donate to the mArtketplace!",
-                amount: 1000,
-                currency: 'aud',
-                quantity: 1,
-            }],
-            payment_intent_data: {
-                metadata: {
-                    user_id: current_user.id,
-                }
-            },
-            success_url: "#{root_url}pages/donated?userId=#{current_user.id}",
-            cancel_url: "#{root_url}"
-        )
-    
-        @session_id = session.id
+        if user_signed_in?
+            session = Stripe::Checkout::Session.create(
+                payment_method_types: ['card'],
+                customer_email: current_user.email,
+                line_items: [{
+                    name: "Donate to the mArtketplace!",
+                    amount: 1000,
+                    currency: 'aud',
+                    quantity: 1,
+                }],
+                payment_intent_data: {
+                    metadata: {
+                        user_id: current_user.id,
+                    }
+                },
+                success_url: "#{root_url}pages/donated?userId=#{current_user.id}",
+                cancel_url: "#{root_url}"
+            )
+        
+            @session_id = session.id
+        end
     end
 end
